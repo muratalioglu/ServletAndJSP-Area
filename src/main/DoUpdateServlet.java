100440406ecb1366eeb3d48642811358fc518a08
murat <mdenizalioglu@gmail.com>
2018-11-15 02:33:50 +0300

    fix broken image problem

--- src/main/DoUpdateServlet.java
+++ src/main/DoUpdateServlet.java
@@ -22,11 +22,18 @@ public class DoUpdateServlet extends HttpServlet {
                     Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
                     final String connectionString = "jdbc:mysql://localhost/tododb?user=root&password=1D952tnZ";
                     conn = DriverManager.getConnection(connectionString);
-                    statement = conn.prepareStatement("UPDATE products SET name = ?, stock = ?, img_name = ? WHERE id = ?;");
+                    
+                    String imgName = request.getParameter("imgName");                    
+                    if (!(imgName.toUpperCase().endsWith(".JPG") || imgName.toUpperCase().endsWith(".JPEG"))) {
+                        statement = conn.prepareStatement("UPDATE products SET name = ?, stock = ? WHERE id = ?;");
+                        statement.setInt(3, Integer.parseInt(request.getParameter("id")));
+                    } else {
+                        statement = conn.prepareStatement("UPDATE products SET name = ?, stock = ?, img_name = ? WHERE id = ?;");
+                        statement.setString(3, request.getParameter("imgName"));
+                        statement.setInt(4, Integer.parseInt(request.getParameter("id")));
+                    }
                     statement.setString(1, request.getParameter("productName"));
-                    statement.setInt(2, Integer.parseInt(request.getParameter("stock")));
-                    statement.setString(3, request.getParameter("imgName"));
-                    statement.setInt(4, Integer.parseInt(request.getParameter("id")));
+                    statement.setInt(2, Integer.parseInt(request.getParameter("stock")));                    
                     if (statement.executeUpdate() == 1) {
                         response.sendRedirect("updateSuccessful.html");
                     } else {
