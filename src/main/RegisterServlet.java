644bfaa40c9b7f2c7e0b244454cc20e453b8ef5a
murat <mdenizalioglu@gmail.com>
2018-11-16 21:44:09 +0300

    Fix emtpy input in register page

--- src/main/RegisterServlet.java
+++ src/main/RegisterServlet.java
@@ -16,43 +16,47 @@ public class RegisterServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html");
-        String name = request.getParameter("name");
-        String password = request.getParameter("password");
-        
-        Connection conn = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            final String connectionString = "jdbc:mysql://localhost/tododb?user=root&password=1D952tnZ";
-            conn = DriverManager.getConnection(connectionString);
-            statement = conn.prepareStatement("SELECT id FROM users WHERE name = ?;");
-            statement.setString(1, name);
-            resultSet = statement.executeQuery();
-            if (!resultSet.next()) {
-                statement.close();
-                statement = conn.prepareStatement("INSERT INTO users (name, password) VALUES (?, ?);");
-                statement.setString(1, name);
-                statement.setString(2, password);
-                if (statement.executeUpdate() == 1) {
-                    response.sendRedirect("registrationSuccessful.html");
-                } else {
-                    response.sendRedirect("registrationFailed.html");
-                }
-            }
-            
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        } finally {
+        if (request.getParameter("name") == null || request.getParameter("password") == null) {
+            response.sendRedirect("registrationFailed.html");
+        } else if (request.getParameter("name").length() == 0 || request.getParameter("password").length() == 0) {
+            response.sendRedirect("registrationFailed.html");
+        } else {
+            String name = request.getParameter("name");
+            String password = request.getParameter("password");            
+            Connection conn = null;
+            PreparedStatement statement = null;
+            ResultSet resultSet = null;
             try {
-                if (conn != null)
-                    conn.close();
-                if (statement != null)
+                Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+                final String connectionString = "jdbc:mysql://localhost/tododb?user=root&password=1D952tnZ";
+                conn = DriverManager.getConnection(connectionString);
+                statement = conn.prepareStatement("SELECT id FROM users WHERE name = ?;");
+                statement.setString(1, name);
+                resultSet = statement.executeQuery();
+                if (!resultSet.next()) {
                     statement.close();
-            } catch (SQLException ex) {
+                    statement = conn.prepareStatement("INSERT INTO users (name, password) VALUES (?, ?);");
+                    statement.setString(1, name);
+                    statement.setString(2, password);
+                    if (statement.executeUpdate() == 1) {
+                        response.sendRedirect("registrationSuccessful.html");
+                    } else {
+                        response.sendRedirect("registrationFailed.html");
+                    }
+                }
+                
+            } catch (Exception ex) {
                 ex.printStackTrace();
-            }
+            } finally {
+                try {
+                    if (conn != null)
+                        conn.close();
+                    if (statement != null)
+                        statement.close();
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }            
         }
     }
 }
