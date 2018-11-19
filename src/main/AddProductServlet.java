df1d46e2e0027196e9165fdcc96f7e46501f3643
murat <mdenizalioglu@gmail.com>
2018-11-19 21:24:18 +0300

    Remove try-catch

--- src/main/AddProductServlet.java
+++ src/main/AddProductServlet.java
@@ -11,30 +11,22 @@ import javax.servlet.http.HttpServletResponse;
 public class AddProductServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         
-        if (!parametersAreNull(request) && !parametersAreEmpty(request)) {
+        if (!parametersAreEmpty(request)) {
             String productName = request.getParameter("productName");
             int stock = Integer.parseInt(request.getParameter("stock"));
             String imgName = request.getParameter("imgName");
             
             Product product = new Product(productName, stock, imgName);
-            try {
-                ProductDAOImpl productDAOImpl = new ProductDAOImpl();
-                productDAOImpl.getConnection();
-                if (productDAOImpl.add(product)) {
-                    response.sendRedirect("addProductSuccessful.html");
-                } else {
-                    response.sendRedirect("addProductFailed.html");
-                }
-            } catch (Exception e) {
-                System.out.println(e);
+            ProductDAOImpl productDAOImpl = new ProductDAOImpl();
+            productDAOImpl.getConnection();
+            if (productDAOImpl.add(product)) {
+                response.sendRedirect("addProductSuccessful.html");
+            } else {
+                response.sendRedirect("addProductFailed.html");
             }
         } else {
             response.sendRedirect("addProductFailed.html");
         }     
-    }
-    
-    private boolean parametersAreNull(HttpServletRequest r) {
-        return r.getParameter("productName") == null || r.getParameter("stock") == null || r.getParameter("imgName") == null;
     }
     
     private boolean parametersAreEmpty(HttpServletRequest r) {
