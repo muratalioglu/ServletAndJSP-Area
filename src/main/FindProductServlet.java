d613124064b233eb0630bf416f34b01eda9ce617
murat <mdenizalioglu@gmail.com>
2018-11-20 02:20:12 +0300

    add FindProductServlet

---
+++ src/main/FindProductServlet.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FindProductServlet extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try {
+            final int id = Integer.parseInt(request.getParameter("id"));
+            ProductDAO productDAO = new ProductDAO();
+            productDAO.getConnection();
+            Product product = productDAO.get(id);
+            if (product != null) {
+                request.setAttribute("product", product);
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("updateProduct.jsp");
+                requestDispatcher.forward(request, response);
+            } else {
+                response.sendRedirect("index.jsp");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
