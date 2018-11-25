eaf286145ea4a99123b0d057a23d1d70412a8f32
murat <mdenizalioglu@gmail.com>
2018-11-25 22:22:51 +0300

    ud AddProductServlet

--- src/controller/AddProductServlet.java
+++ src/controller/AddProductServlet.java
@@ -3,7 +3,6 @@ package controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import dao.ProductDAO;
 import model.Product;
 
-@WebServlet("/addProduct")
 public class AddProductServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
